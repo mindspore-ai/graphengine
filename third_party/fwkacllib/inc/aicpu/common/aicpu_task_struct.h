38c74ab5eced0af7ceeca6c59030778715bb8c9e
yanghaoran <yanghaoran2@huawei.com>
2020-06-20 11:09:51 +0800

    modify aicpu task definition

--- third_party/fwkacllib/inc/aicpu/common/aicpu_task_struct.h
+++ third_party/fwkacllib/inc/aicpu/common/aicpu_task_struct.h
@@ -25,6 +25,8 @@ struct AicpuParamHead
 {
     uint32_t        length;                    // Total length: include cunstom message
     uint32_t        ioAddrNum;                 // Input and output address number
+    uint32_t        extInfoLength;             // extInfo struct Length
+    uint64_t        extInfoAddr;               // extInfo address
 } __attribute__ ((packed));
 
 }  // namespace aicpu

395a4a8a31a457bc4c42d05e5a8ea4982ce99d29
wxl <wanxuelei@huawei.com>
2020-12-17 21:21:14 +0800

    bugfix

--- ge/ir_build/ge_ir_build.cc
+++ ge/ir_build/ge_ir_build.cc
@@ -318,6 +318,13 @@ graphStatus Impl::CheckOptions(const std::map<std::string, std::string> &options
   if (it != options_.end() && (CheckDisableReuseMemoryParamValid(it->second) != GRAPH_SUCCESS)) {
     return GRAPH_PARAM_INVALID;
   }
+  // Check Input Format
+  if (options_.find(kInputFormat) != options_.end()) {
+    if (!options_[kInputFormat].empty() && !ge::TypeUtils::IsFormatValid(options_[kInputFormat].c_str())) {
+      GELOGE(ge::PARAM_INVALID, "user input format [%s] is not found!", options_[kInputFormat].c_str());
+      return GRAPH_PARAM_INVALID;
+    }
+  }
   return GRAPH_SUCCESS;
 }
 

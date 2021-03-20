e1eb148756b27dc87d836a60e73afdcc0d098c56
unknown <zhaozhixuan2@hisilicon.com>
2021-03-20 18:03:08 +0800

    Fix bug of const input index.

--- inc/framework/generator/ge_generator.h
+++ inc/framework/generator/ge_generator.h
@@ -99,6 +99,7 @@ class GE_FUNC_VISIBILITY GeGenerator {
                        const string &model_file_name, OpEngineType engine_type, ModelBufferData &model_buff,
                        bool is_offline = true);
   bool CheckNoAicore(const ComputeGraphPtr &graph);
+  void RemoveConst(const vector<GeTensor> &inputs, vector<GeTensor> &outputs);
   Status CheckForSingleOp(OpDescPtr &op_desc, const vector<GeTensor> &inputs, const vector<GeTensor> &outputs);
 
   using GeRootModelPtr = std::shared_ptr<ge::GeRootModel>;

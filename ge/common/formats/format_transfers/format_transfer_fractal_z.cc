f1039e62bedcfbc536b1354d5dc81ac793da8167
zk <694972388@qq.com>
2021-03-22 19:47:35 +0800

    Transdata for va

--- ge/common/formats/format_transfers/format_transfer_fractal_z.cc
+++ ge/common/formats/format_transfers/format_transfer_fractal_z.cc
@@ -25,7 +25,6 @@
 #include "framework/common/debug/ge_log.h"
 #include "framework/common/debug/log.h"
 #include "graph/utils/type_utils.h"
-#include "iostream"
 namespace ge {
 namespace formats {
 namespace {
@@ -304,7 +303,7 @@ Status TransFormatHwcnToFzWithGroups(const TransArgs &args, TransResult &result,
       return ACL_ERROR_GE_MEMORY_ALLOCATION;);
   ret = memset_s(dst.get(), size_output_data, 0, size_output_data);
   if (ret != EOK) {
-      GELOGE(ACL_ERROR_GE_MEMORY_OPERATE_FAILED, "Failed to operate the dst memory, ret %d", ret);
+      GELOGE(ACL_ERROR_GE_MEMORY_OPERATE_FAILED, "Failed to operate the dst memory, ret is %d", ret);
       return ACL_ERROR_GE_MEMORY_OPERATE_FAILED;
   }
   for (int64_t g = 0; g < groups; g++) {

e943ac3eabe8663a20bb5e5cf18b6c2e720e4ca1
liyihan2@huawei.com <liyihan2@huawei.com>
2021-03-23 17:31:33 +0800

    Common log optimize

--- ge/common/debug/memory_dumper.cc
+++ ge/common/debug/memory_dumper.cc
@@ -41,14 +41,16 @@ FMK_FUNC_HOST_VISIBILITY FMK_FUNC_DEV_VISIBILITY Status MemoryDumper::DumpToFile
   GE_CHECK_NOTNULL(filename);
   GE_CHECK_NOTNULL(data);
   if (len == 0) {
-    GELOGE(FAILED, "len is 0.");
+    GELOGE(FAILED, "[Dump][Data]Init failed, data length is 0.");
+    REPORT_INPUT_ERROR("E10054", std::vector<std::string>({"length"}), std::vector<std::string>({std::to_string(len)}));
     return PARAM_INVALID;
   }
 
   // Open the file
   int fd = OpenFile(filename);
   if (fd == kInvalidFd) {
-    GELOGE(FAILED, "Open file failed.");
+    GELOGE(FAILED, "[Dump][Data]Open file failed, filename:%s.", filename.c_str());
+     REPORT_INPUT_ERROR("E10055", std::vector<std::string>({"filename"}), std::vector<std::string>({filename.c_str()}));
     return FAILED;
   }
 
@@ -57,13 +59,13 @@ FMK_FUNC_HOST_VISIBILITY FMK_FUNC_DEV_VISIBILITY Status MemoryDumper::DumpToFile
   int32_t mmpa_ret = mmWrite(fd, data, len);
   // mmWrite return -1:Failed to write data to file；return -2:Invalid parameter
   if (mmpa_ret == EN_ERROR || mmpa_ret == EN_INVALID_PARAM) {
-    GELOGE(FAILED, "Write to file failed. errno = %d, %s", mmpa_ret, strerror(errno));
+    GELOGE(FAILED, "[Dump][Data]Write data to file failed. errno = mmpa_ret:%d, error:%s", mmpa_ret, strerror(errno));
     ret = FAILED;
   }
 
   // Close the file
   if (mmClose(fd) != EN_OK) {  // mmClose return 0: success
-    GELOGE(FAILED, "Close file failed.");
+    GELOGE(FAILED, "[Dump][Data]Close file failed, error_code:%u.", ret);
     ret = FAILED;
   }
 
@@ -89,7 +91,8 @@ FMK_FUNC_HOST_VISIBILITY FMK_FUNC_DEV_VISIBILITY Status MemoryDumper::Open(const
 
   fd_ = OpenFile(filename);
   if (fd_ == kInvalidFd) {
-    GELOGE(FAILED, "Open %s failed.", filename);
+    GELOGE(FAILED, "[Open][File]Open file:%s failed.", filename);
+    REPORT_INNER_ERROR("E19999", "Open file:%s failed.", filename)
     return FAILED;
   }
 
@@ -104,7 +107,7 @@ FMK_FUNC_HOST_VISIBILITY FMK_FUNC_DEV_VISIBILITY Status MemoryDumper::Dump(void 
   int32_t mmpa_ret = mmWrite(fd_, data, len);
   // mmWrite return -1:failed to write data to file；return -2:invalid parameter
   if (mmpa_ret == EN_ERROR || mmpa_ret == EN_INVALID_PARAM) {
-    GELOGE(FAILED, "Write to file failed. errno = %d, %s", mmpa_ret, strerror(errno));
+    GELOGE(FAILED, "[Dump][Data]Write data to file failed, errno = mmpa_ret:%d, error:%s", mmpa_ret, strerror(errno));
     return FAILED;
   }
 
@@ -157,7 +160,7 @@ int MemoryDumper::OpenFile(const char *filename) {
 
   int32_t fd = mmOpen2(real_path.c_str(), M_RDWR | M_CREAT | O_TRUNC, mode);
   if (fd == EN_ERROR || fd == EN_INVALID_PARAM) {
-    GELOGE(kInvalidFd, "open file failed. errno = %d, %s", fd, strerror(errno));
+    GELOGE(kInvalidFd, "[Open][File]Failed. errno = %d, %s", fd, strerror(errno));
     return kInvalidFd;
   }
   return fd;

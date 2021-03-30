1e04494d0e10387be302943ec7a892a66e151da5
liyihan2@huawei.com <liyihan2@huawei.com>
2021-03-30 11:26:47 +0800

    common formats log optimize

--- ge/common/formats/format_transfers/format_transfer_c1hwncoc0_hwcn.cc
+++ ge/common/formats/format_transfers/format_transfer_c1hwncoc0_hwcn.cc
@@ -154,7 +154,7 @@ Status FormatTransferC1hwncoc0Hwcn::TransFormat(const TransArgs &args, TransResu
     }
     GELOGE(ACL_ERROR_GE_SHAPE_INVALID, "[Get][Shape]Failed, total size %ld from dst shape %s, src shape %s.",
            total_size, ShapeToString(args.dst_shape).c_str(), ShapeToString(args.src_shape).c_str());
-    REPORT_CALL_ERROR("E19999", "Get shape faield, total size %la from dst shape %s, src shape %s.",
+    REPORT_CALL_ERROR("E19999", "Get shape faield, total size %ld from dst shape %s, src shape %s.",
 		      total_size, ShapeToString(args.dst_shape).c_str(), ShapeToString(args.src_shape).c_str());
     return ACL_ERROR_GE_SHAPE_INVALID;
   }

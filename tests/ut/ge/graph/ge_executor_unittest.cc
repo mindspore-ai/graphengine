28ed441b42e13b83ce8594e7b6b0f74705fe27b7
zhaozhixuan <zhaozhixuan2@hisilicon.com>
2021-04-29 21:28:27 +0800

    run_graph_with_stream.

--- tests/ut/ge/graph/ge_executor_unittest.cc
+++ tests/ut/ge/graph/ge_executor_unittest.cc
@@ -323,3 +323,4 @@ TEST_F(UtestGeExecutor, get_op_attr) {
   ret = ge_executor.GetOpAttr(3, "test", ge::ATTR_NAME_DATA_DUMP_ORIGIN_OP_NAMES, attr_value);
   EXPECT_EQ(ret, ACL_ERROR_GE_EXEC_MODEL_ID_INVALID);
 }
+}

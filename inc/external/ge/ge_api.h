03168ff877fe61d0490d5ca3f57ebebb02523f6e
zhou_chao1993 <zhouchao46@huawei.com>
2021-05-07 17:08:29 +0800

    pclint

--- inc/external/ge/ge_api.h
+++ inc/external/ge/ge_api.h
@@ -142,7 +142,8 @@ class GE_FUNC_VISIBILITY Session {
   ///
   Status BuildGraph(uint32_t graphId, const std::vector<InputTensorInfo> &inputs);
 
-  Status BuildGraph(uint32_t graphId, const std::vector<ge::Tensor> &inputs);  // lint !e720
+  Status BuildGraph(uint32_t graphId, const std::vector<ge::Tensor> &inputs);  /*lint !e148*/
+
   ///
   /// @ingroup ge_graph
   /// @brief run graph in the session with specific session id asynchronously

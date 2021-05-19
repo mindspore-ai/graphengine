2083a8e059de8ddfbf6d19bdd6ed398f2463c723
lichun <lichun30@hisilicon.com>
2021-05-19 11:30:16 +0800

    fwkacllib support inference when ge lib is not inited

--- ge/hybrid/node_executor/aicore/aicore_task_compiler.h
+++ ge/hybrid/node_executor/aicore/aicore_task_compiler.h
@@ -18,7 +18,6 @@
 #define GE_HYBRID_KERNEL_AICORE_TASK_COMPILER_H_
 
 #include <mutex>
-#include <atomic>
 #include "opskernel_manager/ops_kernel_manager.h"
 #include "aicore_node_executor.h"
 
@@ -35,7 +34,7 @@ class AiCoreTaskCompiler : public TaskCompiler {
   Status DoCompileOp(const NodePtr &node) const;
   Status DoGenerateTask(const Node &node, std::vector<domi::TaskDef> &tasks);
   OpsKernelInfoStorePtr aic_kernel_store_;
-  std::atomic_bool is_initialized_{false};
+  bool is_initialized_ = false;
   static std::mutex mu_;
 };
 }  // namespace hybrid

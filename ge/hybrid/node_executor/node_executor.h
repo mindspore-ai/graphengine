b64048a39f53e30fcc69491bf0c93c805bdec3b8
xchu42 <chuxing@huawei.com>
2021-06-12 14:01:24 +0800

    Init NodeExecutor on demand

--- ge/hybrid/node_executor/node_executor.h
+++ ge/hybrid/node_executor/node_executor.h
@@ -179,8 +179,6 @@ class NodeExecutorManager {
    */
   Status EnsureInitialized();
 
-  Status InitializeExecutors();
-
   void FinalizeExecutors();
 
   /**
@@ -196,7 +194,7 @@ class NodeExecutorManager {
    * @param executor        executor
    * @return SUCCESS on success, error code otherwise
    */
-  Status GetExecutor(Node &node, const NodeExecutor **executor) const;
+  Status GetExecutor(Node &node, const NodeExecutor **executor);
 
   /**
    * Resolve executor type by node
@@ -206,12 +204,13 @@ class NodeExecutorManager {
   ExecutorType ResolveExecutorType(Node &node) const;
 
  private:
+  Status GetOrCreateExecutor(ExecutorType executor_type, const NodeExecutor **executor);
+
   std::map<ExecutorType, std::unique_ptr<NodeExecutor>> executors_;
   std::map<ExecutorType, std::function<NodeExecutor *()>> builders_;
   std::map<std::string, NodeExecutorManager::ExecutorType> engine_mapping_;
   std::mutex mu_;
   bool initialized_ = false;
-  bool executor_initialized_ = false;
   int ref_count_ = 0;
 };
 
